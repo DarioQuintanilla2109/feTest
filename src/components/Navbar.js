439fa7d91f6b875ff067d9229a10403d12e0092a
Dario_Quintanilla <DarioQuintanilla2@gmail.com>
2021-11-16 00:08:41 -0500

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export default function Navbar() {
+  const [name, setName] = useState('Azora')
+  return (
+    <View style={styles.containerRow}>
+      <View>
+        <Icon name={'ios-caret-back-outline'} color={'white'} size={40} />
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={styles.txtColor}>07:34:28</Text>
+        <Text style={styles.txtSub}>2 picks left</Text>
+      </View>
+      <View>
+        <Text style={styles.txtColor}>{name}</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  containerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 8,
+  },
+  txtColor: {
+    color: 'white',
+    fontSize: 28,
+  },
+  txtSub: {
+    color: 'white',
+    fontSize: 18,
+  },
+})
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,17 +1,29 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import imgBack from '../../assets/imgBack.jpg'
+import Navbar from '../components/Navbar'
 
 export default function home({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>home screen</Text>
-    </SafeAreaView>
+    <ImageBackground
+      imageStyle={{ opacity: 0.6 }}
+      resizeMode='cover'
+      source={imgBack}
+      style={styles.imgStyle}
+    >
+      <SafeAreaView style={styles.container}>
+        <Navbar />
+      </SafeAreaView>
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  imgStyle: {
     flex: 1,
   },
 })
